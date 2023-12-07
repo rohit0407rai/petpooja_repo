e53cde2d5440f28349c8de79bd553474f77d0780
rohit0407rai <rohit0407rai@gmail.com>
2023-12-08 00:06:05 +0530

    Debug Changes

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express();
 const port = 3000; 
 
 app.use(bodyParser.json());
-const STATIC_BEARER_TOKEN = 'Bearer 77d865e9015053e539a90ce9964afacfa45e4acd31a1b996ea70b7bfacd0d67f';
+app.use(bodyParser.json({ limit: '10mb' }));
+ const STATIC_BEARER_TOKEN = 'Bearer 77d865e9015053e539a90ce9964afacfa45e4acd31a1b996ea70b7bfacd0d67f';
 const STATIC_CLIENT_ID = '2c32f0d647472abac59c80d57f4b92fa96aa569b5f56925daa29cae919f57e3e';
 
 app.use(cors());
@@ -58,14 +59,14 @@ app.post('/getStoreStatus', (req, res) => {
   res.json({ success: true, message: 'Store status getted successfully' });
 });
 app.post('/updateStoreStatus', (req, res) => {
-  const { restID, status, store_status, turn_on_time } = req.body;
+  const { restID,  store_status,reason, turn_on_time } = req.body;
 
   // Validate the required parameters
-  if (!restID || !status || !store_status || (status === 'failed' && !turn_on_time)) {
-      return res.status(400).json({ success: false, message: 'Missing required parameters' });
-  }
+  // if (!restID || !status || !store_status || (status === 'failed' && !turn_on_time)) {
+  //     return res.status(400).json({ success: false, message: 'Missing required parameters' });
+  // }
   console.log(`Received request for restaurant ID: ${restID}`);
-  console.log(`Status: ${status}`);
+  console.log(`Status: ${reason}`);
   console.log(`store_statis: ${store_status}`);
   console.log(`turn_on_time: ${turn_on_time}`);
   res.status(200).json({ success: true, message: 'Store status updated successfully' });
