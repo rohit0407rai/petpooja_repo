bceb95ed7bfbdc4c61d1914ac226345b4df30b86
rohit0407rai <rohit0407rai@gmail.com>
2023-12-15 11:50:11 +0530

    PRODUCTION_CHANGES

--- index.js
+++ index.js
@@ -56,16 +56,33 @@ app.post('/petpooja/menu/update', (req, res) => {
     const requestData = req.body;
     console.log("Hawai dunia");
     console.log('Request data:', requestData);
-    res.json({ success: true, message: 'Menu updated successfully' });
+    res.json({ success: "1", message: 'Menu items are successfully listed.' });
   } catch (error) {
     // Handle errors
     console.error('Error processing Petpooja data:', error);
-    res.status(500).json({ success: false, message: 'Internal Server Error' });
+    res.status(500).json({ success: "0", message: 'Internal Server Error' });
   }
 });
+// app.post('/toggleStockStatus', (req, res) => {
+//   // Extract data from the request body
+//   const { restID, type, inStock, itemID, autoTurnOnTime,customTurnOnTime } = req.body;
+
+//   // Perform necessary actions based on the data
+//   // (You can replace the console.log statements with your actual logic)
+
+//   console.log(`Received request for restaurant ID: ${restID}`);
+//   console.log(`Type: ${type}`);
+//   console.log(`In stock: ${inStock}`);
+//   console.log(`AutoTurnOnTime: ${autoTurnOnTime}`);
+//   console.log(`CustomTurnOnTime: ${customTurnOnTime}`);
+//   console.log(`Item IDs: ${itemID.join(', ')}`);
+
+//   // Respond with a success message
+//   res.json({ success: '1', message: 'Stock status updated successfully' });
+// });
 app.post('/toggleStockStatus', (req, res) => {
   // Extract data from the request body
-  const { restID, type, inStock, itemID, autoTurnOnTime,customTurnOnTime } = req.body;
+  const { restID, type, inStock, itemID, autoTurnOnTime, customTurnOnTime } = req.body;
 
   // Perform necessary actions based on the data
   // (You can replace the console.log statements with your actual logic)
@@ -75,31 +92,73 @@ app.post('/toggleStockStatus', (req, res) => {
   console.log(`In stock: ${inStock}`);
   console.log(`AutoTurnOnTime: ${autoTurnOnTime}`);
   console.log(`CustomTurnOnTime: ${customTurnOnTime}`);
-  console.log(`Item IDs: ${itemID.join(', ')}`);
+  if (itemID !== undefined) {
+    if (Array.isArray(itemID)) {
+      console.log(`Item IDs: ${itemID.join(', ')}`);
+    } else {
+      console.log(`Item ID: ${itemID}`);
+    }
+  } else {
+    console.log('Item ID not provided');
+  }
+  try {
+    // Your logic here
 
-  // Respond with a success message
-  res.json({ success: true, message: 'Stock status updated successfully' });
+    // If the logic is successful, respond with a success message
+    res.json({code: '200', status: 'success', message: 'Stock status updated successfully' });
+  } catch (error) {
+    // If there is an error, respond with an error message
+    console.error(`Error processing request: ${error.message}`);
+    res.json({code: '500', success: 'failed',  message: 'Error updating stock status' });
+  }
 });
+
 app.post('/getStoreStatus', (req, res) => {
   const { restID, status, store_status } = req.body;
   console.log(`Received request for restaurant ID: ${restID}`);
   console.log(`Status: ${status}`);
   console.log(`store_statis: ${store_status}`);
-  res.json({ success: true, message: 'Store status getted successfully' });
+  res.json({ success: '1', message: 'Store status getted successfully' });
 });
+// app.post('/updateStoreStatus', (req, res) => {
+//   const { restID,  store_status,reason, turn_on_time } = req.body;
+
+//   // Validate the required parameters
+//   // if (!restID || !status || !store_status || (status === 'failed' && !turn_on_time)) {
+//   //     return res.status(400).json({ success: false, message: 'Missing required parameters' });
+//   // }
+//   console.log(`Received request for restaurant ID: ${restID}`);
+//   console.log(`Status: ${reason}`);
+//   console.log(`store_statis: ${store_status}`);
+//   console.log(`turn_on_time: ${turn_on_time}`);
+//   res.status(200).json({ success: '1', message: 'Store status updated successfully' });
+// });
 app.post('/updateStoreStatus', (req, res) => {
-  const { restID,  store_status,reason, turn_on_time } = req.body;
+  const { restID, store_status, reason, turn_on_time } = req.body;
 
   // Validate the required parameters
   // if (!restID || !status || !store_status || (status === 'failed' && !turn_on_time)) {
   //     return res.status(400).json({ success: false, message: 'Missing required parameters' });
   // }
+  
   console.log(`Received request for restaurant ID: ${restID}`);
   console.log(`Status: ${reason}`);
-  console.log(`store_statis: ${store_status}`);
+  console.log(`store_status: ${store_status}`);
   console.log(`turn_on_time: ${turn_on_time}`);
-  res.status(200).json({ success: true, message: 'Store status updated successfully' });
+
+  try {
+    // Your logic here
+
+    // If the logic is successful, respond with a success message including restID
+    res.status(200).json({ http_code: '200', status: 'success', message: `Store Status updated successfully for store ${restID}` });
+  } catch (error) {
+    // If there is an error, respond with an error message
+    console.error(`Error processing request: ${error.message}`);
+    res.status(500).json({  http_code: '500',status: 'success', message: 'Error updating store status' });
+  }
 });
+
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
