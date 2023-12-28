f4fb29079c4bfe28df06d89b4c54acd84a072a2a
rohit0407rai <rohit0407rai@gmail.com>
2023-12-28 16:20:06 +0530

    final 3

--- index.js
+++ index.js
@@ -211,13 +211,13 @@ app.post('/receiveOrder', async (req, res) => {
     console.log('Response from other server:', response.data);
 
     res.status(200).json({
-      success: true,
+      success: '1',
       message: 'Order data received and forwarded successfully.',
       responseData: response.data, 
     });
   } catch (error) {
     console.error('Error processing order data:', error);
-    res.status(500).json({ success: false, message: 'Internal server error.' });
+    res.status(500).json({ success: '0', message: 'Internal server error.' });
   }
 });
 
