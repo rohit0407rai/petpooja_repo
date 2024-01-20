f0410edde9dc631d1ff8e1a12ddbdc58c492b518
rohit0407rai <rohit0407rai@gmail.com>
2024-01-20 10:33:05 +0530

    8th production chnages

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors'); 
 const axios= require('axios');
-const { default: fetch } = require('node-fetch');
+import fetch from 'node-fetch';
 const app = express();
 const port = 3000; 
 
